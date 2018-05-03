744a69e5821ede122d257bfb8b1d77351d768654
laoboxie <594502135@qq.com>
2018-05-03 09:51:20 +0800

    增加 blog模型
    增加 blog模型

--- models.py
+++ models.py
@@ -15,7 +15,11 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now-datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
+# 查询测试
 class Choice(models.Model):
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -23,4 +27,32 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE,related_name='entries')
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline
         
