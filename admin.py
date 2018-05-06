7fee5f157c30d4b183e808640a383d2066c0cbbd
laoboxie <594502135@qq.com>
2018-05-06 22:02:47 +0800

    修改
    没有过多的修改

--- admin.py
+++ admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 
 from . import models
+import datetime
+from django.utils import timezone
 # Register your models here.
 
 class ChoiceInline(admin.TabularInline):
 	model = models.Choice
 	extra = 0
 
+@admin.register(models.Question)
 class QuestionAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None,					{'fields':['question_text']}),
@@ -17,12 +20,18 @@ class QuestionAdmin(admin.ModelAdmin):
 	list_filter = ['pub_date',]
 	search_fields = ['question_text',]
 
-admin.site.register(models.Question,QuestionAdmin)
+	def was_published_recently(self,obj):
+	    now = timezone.now()
+	    return now-datetime.timedelta(days=1) <= obj.pub_date <= now
+	was_published_recently.admin_order_field = 'pub_date'
+	was_published_recently.boolean = True
+	was_published_recently.short_description = 'Published recently?'
 
+@admin.register(models.Entry)
 class EntryAdmin(admin.ModelAdmin):
-	pass
+	exclude = ('mod_date',)
 
-admin.site.register(models.Entry,EntryAdmin)
+# admin.site.register(models.Entry,EntryAdmin)
 admin.site.register(models.Author)
 admin.site.register(models.Blog)
 
